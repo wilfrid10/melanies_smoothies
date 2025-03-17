e3b280616951e8de31b6a10922175547b1a870a6
wilfrid10 <72437996+wilfrid10@users.noreply.github.com>
2025-03-18 00:57:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,11 @@ st.write('The name of your smoothier will be',name_on_order)
 
 #ajouter les 25 fruits a ma liste à partir de la BD
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe,use_container_width=True)
+#st.dataframe(data=my_dataframe,use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
